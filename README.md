e0772d11cade5542833d6d4672e5ce2b64b0d828
Nguyen Duy Quang <43096050+YumeAyasaki@users.noreply.github.com>
2023-10-10 00:21:18 +0700

    Face detection
    Base view for face detection

---
+++ README.md
@@ -0,0 +1,12 @@
+Command to install things:
+conda install django
+conda install postgresql
+conda install -c conda-forge django-environ
+conda install psycopg2
+conda install Pillow
+conda install -c conda-forge opencv
+
+Command to start:
+python manage.py migrate
+python manage.py collectstatic
+python manage.py runserver
--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 
+import cv2
+
 # Create your views here.
 def login_user(request):
     username = request.POST['username']
@@ -39,7 +41,6 @@ def logout_user(request):
 
 @login_required
 def upload_image(request):
-    print(request.FILES.__dict__)
     image = request.FILES['profile_image']
 
     user = request.user
@@ -47,3 +48,25 @@ def upload_image(request):
     user.save()
 
     return HttpResponseRedirect('/')
+
+@login_required
+def face_detection(request):
+    user = request.user
+    original_image = user.profile_image
+
+    def face_bounding_box(image):
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+            cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
+        
+        return image
+    
+    face_image = face_bounding_box(original_image)
+    
+    # Return as a response
+    response = HttpResponse(content_type='image/jpeg')
+    cv2.imwrite(response, face_image)
+    return response
